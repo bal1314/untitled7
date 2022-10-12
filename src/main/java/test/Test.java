8b629a2bd81cb51f8f55b3456fdd50a8961d7b4d
橙子薄荷 <b2518698537@163.com>
2022-10-12 10:57:24 +0800

    最后push

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -4,6 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Herld");
         System.out.println("dsfsffa");
-        System.out.println("bail");
+
     }
 }
